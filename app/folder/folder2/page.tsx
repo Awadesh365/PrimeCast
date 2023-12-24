6f5944327911d1a350a615de9696443adf41ef6d
Awadesh <36awadesh@gmail.com>
2023-12-24 22:45:54 +0530

    TEST Complete: Remove nested routes

--- app/folder/folder2/page.tsx
+++
@@ -1,9 +0,0 @@
-const Folder2Page = () => {
-    return (
-        <div>
-            Test my new Page sub route
-        </div>
-    );
-};
-
-export default Folder2Page;
--- app/folder/page.tsx
+++
@@ -1,9 +0,0 @@
-const FolderPage = () => {
-    return (
-        <div>
-            Test my new Page route
-        </div>
-    );
-};
-
-export default FolderPage;
