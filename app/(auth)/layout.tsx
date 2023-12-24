dce5d240008a9e027f9a0b7969453df711ce99fe
Awadesh <36awadesh@gmail.com>
2023-12-25 01:18:58 +0530

    Authentication completed also  the center allingment of singin/signup page is done.

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { Children } from "react";
+
+const AuthLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="h-full flex items-center
+            justify-center">
+            {children}
+        </div>
+    );
+};
+
+export default AuthLayout;
