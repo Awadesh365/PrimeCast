dacdc35ef44a2b64ba3682e71699defe26bbc04b
Awadesh <36awadesh@gmail.com>
2023-12-24 23:21:04 +0530

    Create Reusable Layout for Auth

---
+++ app/auth/layout.tsx
@@ -0,0 +1,12 @@
+const AuthLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
+export default AuthLayout;
