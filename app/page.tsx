2054f927a0b36f7560391b59d00a2bddb35666ff
Awadesh <36awadesh@gmail.com>
2023-12-24 21:50:37 +0530

    Import Button in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 
 export default function Home() {
   return (
-    <p className='text-red-500 font-bold'
-    >Testing Live stream for Tailwind Config</p>
+    <Button>Click me</Button>
   )
 }
