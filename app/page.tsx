b989c6e6a77f75055c8abc5b523a63cc81480efd
Awadesh <36awadesh@gmail.com>
2023-12-24 22:03:11 +0530

    TEST complete : Custom removed

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <Button size="lg" variant="custom" >Click me</Button>
+    <Button size="lg">Click me</Button>
   )
 }
