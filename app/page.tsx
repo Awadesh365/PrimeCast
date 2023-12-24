d387ff8529891ec5d463fb668c23dc33515cefdf
Awadesh <36awadesh@gmail.com>
2023-12-24 21:33:47 +0530

    Test For Tailwind Configuration

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    
+    <p className='text-red-500 font-bold'
+    >Testing Live stream for Tailwind Config</p>
   )
 }
