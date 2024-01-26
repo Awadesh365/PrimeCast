b681a18933a7a963956682c8e6251a3de71cf8a2
Awadesh <36awadesh@gmail.com>
2024-01-26 20:18:26 +0530

    Dashboard Correction

--- app/(auth)/_components/logo.tsx
+++ app/(auth)/_components/logo.tsx
@@ -4,34 +4,21 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 const font = Poppins({
-    subsets: ["latin"],
-    weight: ["200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
 export const Logo = () => {
-    return (
-        <div className={cn(
-            "flex flex-col items-center gap-y-4"
-        )}>
-            <div className="bg-white rounded-full p-1">
-                <Image
-                    src="/spooky.svg"
-                    alt="GameHub"
-                    height="80"
-                    width="80"
-                />
-            </div>
+  return (
+    <div className={cn("flex flex-col items-center gap-y-4")}>
+      <div className="bg-white rounded-full p-1">
+        <Image src="/spooky.svg" alt="GameHub" height="80" width="80" />
+      </div>
 
-            <div className={cn(
-                "flex flex-col items-center",
-                font.className,
-            )}>
-                <p className="text-xl font-semibold">
-                    GameHub
-                </p>
-                <p className="text-sm text-muted-foreground">
-                    Let&apos;s Play</p>
-            </div>
-        </div >
-    );
+      <div className={cn("flex flex-col items-center", font.className)}>
+        <p className="text-xl font-semibold">GameHub</p>
+        <p className="text-sm text-muted-foreground">Let&apos;s Play</p>
+      </div>
+    </div>
+  );
 };
