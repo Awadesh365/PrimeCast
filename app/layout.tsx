a290e4c25cf0dc79679141ba7614d2200fe3990f
Awadesh <36awadesh@gmail.com>
2024-02-03 23:57:49 +0530

    Update Layout app

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
