32d2c1e9a7ab8fa1d9d1d8751e2502a8f1a5d98c
Awadesh <36awadesh@gmail.com>
2024-02-04 00:48:45 +0530

    app browse  layout update

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,18 +1,17 @@
 import { Navbar } from "./_components/navbar";
+import { Sidebar } from "./_components/sidebar";
+import { Container } from "./_components/container";
 
-const BrowseLayout = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  return ( 
+const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
     <>
       <Navbar />
       <div className="flex h-full pt-20">
-        {children}
+        <Sidebar />
+        <Container>{children}</Container>
       </div>
     </>
   );
 };
- 
+
 export default BrowseLayout;
